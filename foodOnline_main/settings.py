366e144c1818937ba9248ad2351710460376efbf
Adnan <acoskun@gmail.com>
2023-12-11 23:07:05 +0300

    aws postgres configuration

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -77,16 +77,33 @@ WSGI_APPLICATION = 'foodOnline_main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
+# Database Configuration
+import os
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+    #    'default': {
+    #        'ENGINE': 'django.db.backends.sqlite3',
+    #        'NAME': BASE_DIR / 'db.sqlite3',
+    #    }
+     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': 'mixl vlkg epug uakr',
         'HOST': config('DB_HOST'),
     }
-}
+    }
 
 AUTH_USER_MODEL = 'accounts.user'
 
